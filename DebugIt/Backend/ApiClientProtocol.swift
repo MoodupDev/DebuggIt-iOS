af15220739a80ff18c0110cd5a738cd6a9ce75e7
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-10-26 12:47:18 +0200

    Update ApiClientProtocol blocks

--- DebugIt/Backend/ApiClientProtocol.swift
+++ DebugIt/Backend/ApiClientProtocol.swift
@@ -6,25 +6,24 @@
 //  Copyright © 2016 Mood Up. All rights reserved.
 //
 
-
 protocol ApiClientProtocol {
     func login(email: String,
                password: String,
-               successBlock: (_ response: String) -> (),
-               errorBlock: (_ error: Error?) -> ()
+               successBlock: @escaping (_ response: String) -> (),
+               errorBlock: @escaping (_ statusCode: Int? , _ body: String?) -> ()
     )
     
     func addIssue(title: String,
                   content: String,
                   priority: String,
                   kind: String,
-                  successBlock: (_ response: String) -> (),
-                  errorBlock: (_ error: Error?) -> ()
+                  successBlock: @escaping (_ response: String) -> (),
+                  errorBlock: @escaping (_ statusCode: Int? , _ body: String?) -> ()
     )
     
     func refreshToken(token: String,
-                      successBlock: (_ response: String) -> (),
-                      errorBlock: (_ error: Error?) -> ()
+                      successBlock: @escaping (_ response: String) -> (),
+                      errorBlock: @escaping (_ statusCode: Int? , _ body: String?) -> ()
     )
     
 }
