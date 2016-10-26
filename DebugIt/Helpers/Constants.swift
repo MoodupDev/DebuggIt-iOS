c220551adaa0c43305fc88ff67011fe93d5b9c9f
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-10-26 12:48:19 +0200

    Update constants

--- DebugIt/Helpers/Constants.swift
+++ DebugIt/Helpers/Constants.swift
@@ -9,7 +9,7 @@
 struct Constants {
     
     struct Bitbucket {
-        let authorizeUrl = "https://bitbucket.org/site/oauth2/access_token"
-        let issuesUrl = "https://api.bitbucket.org/1.0/repositories/%s/%s/issues"
+        static let authorizeUrl = "https://bitbucket.org/site/oauth2/access_token"
+        static let issuesUrl = "https://api.bitbucket.org/1.0/repositories/%s/%s/issues"
     }
 }
