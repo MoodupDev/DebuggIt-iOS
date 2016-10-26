25d547d9e7fd660d4336df5181ec2bf541fd45bf
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-10-26 13:25:15 +0200

    Add GitHub constants

--- DebugIt/Helpers/Constants.swift
+++ DebugIt/Helpers/Constants.swift
@@ -8,8 +8,17 @@
 
 struct Constants {
     
+    static let debuggItUrl = "http://debugg.it"
+    
     struct Bitbucket {
         static let authorizeUrl = "https://bitbucket.org/site/oauth2/access_token"
         static let issuesUrl = "https://api.bitbucket.org/1.0/repositories/%s/%s/issues"
+    }
+    
+    struct GitHub {
+        static let authorizeUrl = "https://api.github.com/authorizations"
+        static let issuesUrl = "https://api.github.com/repos/%s/%s/issues"
+        
+        static let jsonFormat = "application/vnd.github.v3+json"
     }
 }
