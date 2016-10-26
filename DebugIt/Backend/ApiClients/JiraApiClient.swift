83e28a57ed37e6198428b510753504bd8cc098e7
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-10-26 14:13:00 +0200

    Implement JIRA api client methods

--- DebugIt/Backend/ApiClients/JiraApiClient.swift
+++ DebugIt/Backend/ApiClients/JiraApiClient.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2016 Mood Up. All rights reserved.
 //
 
+import Alamofire
+
 class JiraApiClient: ApiClientProtocol {
     
     // MARK: Properties
@@ -28,13 +30,83 @@ class JiraApiClient: ApiClientProtocol {
     
     func login(email: String, password: String, successBlock: @escaping (String) -> (), errorBlock: @escaping (_ statusCode: Int? , _ body: String?) -> ()) {
         
+        let headers: HTTPHeaders = [
+            "Authorization" : authorizationHeader(username: email, password: password)
+        ]
+        
+        let url = checkUrlProtocol(url: String(format: Constants.Jira.configurationUrl, host))
+        
+        Alamofire.request(url, method: .post, encoding: URLEncoding.default, headers: headers).validate().responseString { (response) in
+            switch response.result {
+            case .success(let value):
+                if response.isSuccess() {
+                    successBlock(value)
+                } else {
+                    errorBlock(response.responseCode, value)
+                }
+            case .failure(let error as AFError):
+                errorBlock(nil, error.errorDescription)
+            default:
+                errorBlock(nil, nil)
+                
+            }
+            
+        }
+        
     }
     
     func addIssue(title: String, content: String, priority: String, kind: String, successBlock: @escaping (String) -> (), errorBlock: @escaping (_ statusCode: Int? , _ body: String?) -> ()) {
         
+        let params: Parameters = [
+            "fields" : [
+                "issuetype": [
+                    "name": kind
+                ],
+                "summary": title,
+                "project": [
+                    "key": projectKey
+                ],
+                "description": content,
+                "priority": [
+                    "name": priority
+                ]
+            ]
+        ]
+        
+        let headers: HTTPHeaders = [
+            "Authorization": authorizationHeader(username: username ?? "", password: password ?? "")
+        ]
+        
+        let url = checkUrlProtocol(url: String(format: Constants.Jira.issuesUrl, host))
+        
+        Alamofire.request(url, method: .post, parameters: params, encoding: URLEncoding.default, headers: headers).validate().responseString { (response) in
+            switch response.result {
+            case .success(let value):
+                if response.isSuccess() {
+                    successBlock(value)
+                } else {
+                    errorBlock(response.responseCode, value)
+                }
+            case .failure(let error as AFError):
+                errorBlock(nil, error.errorDescription)
+            default:
+                errorBlock(nil, nil)
+                
+            }
+            
+        }
+
+        
     }
     
     func refreshToken(token: String, successBlock: @escaping (String) -> (), errorBlock: @escaping (_ statusCode: Int? , _ body: String?) -> ()) {
-        
+        // do nothing
+    }
+    
+    private func checkUrlProtocol(url: String) -> String {
+        if !usesHttps {
+            return url.replaceFirst(replace: "s", with: "")
+        }
+        return url
     }
 }
