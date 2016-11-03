95695f89b91ea7b0110ece04921aa3a7a083685c
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-11-03 11:19:31 +0100

    Add actions after ended editing text views

--- DebugIt/Views/BugDescriptionPage2ViewController.swift
+++ DebugIt/Views/BugDescriptionPage2ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class BugDescriptionPage2ViewController: UIViewController {
+    
+    // MARK: Properties
+    @IBOutlet weak var stepsToReproduceTextView: UITextView!
+    @IBOutlet weak var actualBehaviorTextView: UITextView!
+    @IBOutlet weak var expectedBehaviorTextView: UITextView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        stepsToReproduceTextView.delegate = self
+        actualBehaviorTextView.delegate = self
+        expectedBehaviorTextView.delegate = self
         // Do any additional setup after loading the view.
     }
 
@@ -31,5 +39,19 @@ class BugDescriptionPage2ViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+}
 
+extension BugDescriptionPage2ViewController : UITextViewDelegate{
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView == stepsToReproduceTextView {
+            print("steps to reproduce")
+            // TODO: save steps to reproduce to report
+        } else if textView == actualBehaviorTextView {
+            print("actual behavior")
+            // TODO: save actual behavior to report
+        } else {
+            print("expected behavior")
+            // TODO: save expected behavior to report
+        }
+    }
 }
