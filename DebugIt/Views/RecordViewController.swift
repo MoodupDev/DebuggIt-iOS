dab12c9cba5fc92e196354672ea270a5ef75298e
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-11-10 09:34:38 +0100

    Update record view controller, update ui every second

--- DebugIt/Views/RecordViewController.swift
+++ DebugIt/Views/RecordViewController.swift
@@ -11,10 +11,21 @@ import AVFoundation
 
 class RecordViewController: UIViewController {
     
+    // MARK: - Properties
+    
+    @IBOutlet weak var remainingTimeLabel: UILabel!
+    @IBOutlet weak var recordCircle: UIImageView!
+    
+    var remainingTime: Int = 60
+    
     var recordingSession: AVAudioSession!
     var audioRecorder: AVAudioRecorder!
     
-    var audioFilename: URL?
+    var timer = Timer()
+    
+    var audioFilename: URL!
+    
+    // MARK: - Overriden methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +38,7 @@ class RecordViewController: UIViewController {
             recordingSession.requestRecordPermission() { [unowned self] allowed in
                 DispatchQueue.main.async {
                     if allowed {
+                        self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(RecordViewController.updateUi), userInfo: nil, repeats: true)
                         self.startRecording()
                     } else {
                         print("failed to record!")
@@ -37,13 +49,10 @@ class RecordViewController: UIViewController {
             print("catch: failed to record!")
         }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-    func startRecording() {
+    // MARK: - Methods
+    
+    private func startRecording() {
         audioFilename = getDocumentsDirectory().appendingPathComponent("recording.m4a")
         
         let settings = [
@@ -63,7 +72,7 @@ class RecordViewController: UIViewController {
         }
     }
     
-    func getDocumentsDirectory() -> URL {
+    private func getDocumentsDirectory() -> URL {
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         let documentsDirectory = paths[0]
         return documentsDirectory
@@ -77,10 +86,14 @@ class RecordViewController: UIViewController {
         
         if success {
             if let fileData = FileManager().contents(atPath: audioFilename!.relativePath) {
+                let alert = Utils.createLoadingAlert(title: "Uploading audio", message: "Wait for end...")
+                self.present(alert, animated: true, completion: nil)
                 ApiClient.upload(.audio, data: fileData.base64EncodedString(), successBlock: {
+                    alert.dismiss(animated: true, completion: nil)
                     self.performSegue(withIdentifier: "recordUploaded", sender: self)
                 }, errorBlock: { (code, message) in
-                    print(code, message)
+                    alert.dismiss(animated: true, completion: nil)
+                    // TODO: show error message 
                 })
             }
         } else {
@@ -88,21 +101,25 @@ class RecordViewController: UIViewController {
         }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    private func updateUi() {
+        remainingTime -= 1
+        remainingTimeLabel.text = String(format: "00:%02d", remainingTime)
+        recordCircle.alpha = CGFloat(remainingTime % 2)
+        if remainingTime == 0 {
+            stopRecording()
+        }
     }
-    */
+    
+    private func stopRecording() {
+        timer.invalidate()
+        finishRecording(success: true)
+    }
     
     // MARK: - Actions
     
     @IBAction func recordStopped(_ sender: UIButton) {
-        finishRecording(success: true)
+        stopRecording()
     }
     
 
