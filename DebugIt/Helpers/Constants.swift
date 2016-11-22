e2e50208052a4cdca671b9ed452aada67f6c9fc0
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-11-22 16:20:02 +0100

    Update constants

--- DebugIt/Helpers/Constants.swift
+++ DebugIt/Helpers/Constants.swift
@@ -26,26 +26,38 @@ struct Constants {
         
         // MARK: URLs
         
-        static let authorizeUrl = "https://bitbucket.org/site/oauth2/access_token"
+        static let authorizeUrl = "https://bitbucket.org/site/oauth2/authorize"
         static let issuesUrl = "https://api.bitbucket.org/1.0/repositories/%@/%@/issues"
+        static let accessTokenUrl = "https://bitbucket.org/site/oauth2/access_token"
         
         // MARK: UserDefault keys
         
         static let accessTokenKey = "bitbucket_access_token"
         static let refreshTokenKey = "bitbucket_refresh_token"
+        
+        // MARK: Bitbucket consumer keys
+        
+        static let clientId = "Jz9hKhxwAWgRNcS6m8"
+        static let clientSecret = "dzyS7K5mnvcEWFtsS6veUM8RDJxRzwXQ"
     }
     
     struct GitHub {
         
         // MARK: URLs
         
-        static let authorizeUrl = "https://api.github.com/authorizations"
+        static let authorizeUrl = "https://github.com/login/oauth/authorize"
         static let issuesUrl = "https://api.github.com/repos/%@/%@/issues"
+        static let accessTokenUrl = "https://github.com/login/oauth/access_token"
         
         // MARK: UserDefault keys
         
         static let accessTokenKey = "github_access_token"
         static let twoFactorAuthCodeKey = "github_2fa_code"
+        
+        // MARK: GitHub app keys
+        
+        static let clientId = "8aac9632491f7d954664"
+        static let clientSecret = "1b7bdf305e08971b3c95c1cfc06fc05eebd59707"
         
         // MARK: API spec
         
