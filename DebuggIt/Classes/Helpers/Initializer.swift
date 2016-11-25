9a6586a89d60832476ba70260cfe76f542314de5
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-11-25 15:21:49 +0100

    Add initializer - helper to initialize views from pod bundle

---
+++ DebuggIt/Classes/Helpers/Initializer.swift
@@ -0,0 +1,36 @@
+//
+//  Initializer.swift
+//  Pods
+//
+//  Created by Arkadiusz Żmudzin on 25.11.2016.
+//
+//
+
+import Foundation
+import UIKit
+
+class Initializer {
+    
+    private static func bundle(forClass: AnyClass) -> Bundle {
+        let podBundle = Bundle(for: forClass)
+        
+        let bundleURL = podBundle.url(forResource: "DebuggIt", withExtension: "bundle")
+        return Bundle(url: bundleURL!)!
+    }
+
+    static func viewController<T: UIViewController>(_ withClass: T.Type) -> T {
+        return withClass.init(nibName: String(describing: withClass), bundle: bundle(forClass: withClass)) as! T
+    }
+    
+    static func view<T: UIView>(_ withClass: T.Type, owner: Any? = nil) -> UIView {
+        return nib(named: String(describing: withClass)).instantiate(withOwner: owner, options: nil)[0] as! UIView
+    }
+    
+    static func image(named: String) -> UIImage {
+        return UIImage(named: named, in: bundle(forClass: DebuggIt.self), compatibleWith: nil)!
+    }
+    
+    static func nib(named: String) -> UINib {
+        return UINib(nibName: named, bundle: bundle(forClass: DebuggIt.self))
+    }
+}
