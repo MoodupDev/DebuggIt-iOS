1a443d2247cf44324e14ce625c38a15b14b9ec53
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-11-25 15:38:47 +0100

    Add embed method for UIViewController

--- DebuggIt/Classes/Helpers/UIViewController+EmbedInContainer.swift
+++ DebuggIt/Classes/Helpers/UIViewController+EmbedInContainer.swift
@@ -7,3 +7,14 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    func embed(_ viewController: UIViewController, in container: UIView) {
+        self.addChildViewController(viewController)
+        viewController.view.frame = CGRect(x: 0, y: 0, width: container.frame.size.width, height: container.frame.size.height)
+        container.addSubview(viewController.view)
+        viewController.didMove(toParentViewController: self)
+    }
+}
