b3de5635ae1bc35c57c090220fe1712bb810c7b3
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-12-02 15:51:58 +0100

    Move DebuggItViewControllerProtocol to classes group, add @objc annotation

---
+++ DebuggIt/Classes/DebuggItViewControllerProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  DebuggItViewControllerProtocol.swift
+//  Pods
+//
+//  Created by Arkadiusz Żmudzin on 30.11.2016.
+//
+//
+
+import Foundation
+
+@objc public protocol DebuggItViewControllerProtocol {}
