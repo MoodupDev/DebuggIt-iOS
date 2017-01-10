155fe259ad9362214d5cccd068997108edf49825
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2017-01-10 15:20:24 +0100

    Move files

---
+++ DebuggIt/DebuggIt.h
@@ -0,0 +1,19 @@
+//
+//  DebuggIt.h
+//  DebuggIt
+//
+//  Created by Arkadiusz Żmudzin on 10.01.2017.
+//  Copyright © 2017 Mood Up. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DebuggIt.
+FOUNDATION_EXPORT double DebuggItVersionNumber;
+
+//! Project version string for DebuggIt.
+FOUNDATION_EXPORT const unsigned char DebuggItVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DebuggIt/PublicHeader.h>
+
+
---
+++ DebuggItTests/DebuggItTests.swift
@@ -0,0 +1,36 @@
+//
+//  DebuggItTests.swift
+//  DebuggItTests
+//
+//  Created by Arkadiusz Żmudzin on 10.01.2017.
+//  Copyright © 2017 Mood Up. All rights reserved.
+//
+
+import XCTest
+@testable import DebuggIt
+
+class DebuggItTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
